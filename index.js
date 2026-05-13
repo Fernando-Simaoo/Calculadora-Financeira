d7fb35a07e780382377ac266a9083d7572dee87b
Fernando <fernando.simao.pro@gmail.com>
2026-05-13 16:49:25 -0300

    basico do basico v1.0.0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 alert("Arquivo conectado!");
+
+
+function inserir(id) {
+    var calc_buffer = document.getElementById("tela");
+
+    var char = document.getElementById(id).innerHTML;
+    calc_buffer.textContent += char;
+}
+
+function backspace() {
+    var calc_buffer = document.getElementById("tela");
+    calc_buffer.textContent = calc_buffer.textContent.slice(0, -1);
+}
+
+function limpar() {
+    var calc_buffer = document.getElementById("tela");
+    calc_buffer.textContent = "";
+}
+
+function calcular() {
+    var calc_buffer = document.getElementById("tela");
+    calc_buffer.textContent = eval(calc_buffer.textContent);
+}
